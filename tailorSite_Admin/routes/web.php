4ec132defc6bc1d8f4f2711c8463e1428ec1ae1c
kelanialiyu <kelanialiyu@gmail.com>
2021-11-25 16:52:15 +0100

    Added Laravel/Breeze

--- tailorSite_Admin/routes/web.php
+++ tailorSite_Admin/routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
-Route::get('login', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
